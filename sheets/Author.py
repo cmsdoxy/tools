5cc926e9607d44056a9ebd9238fb5ff85b7fba72
Mantas <cmsdoxy@cern.ch>
2013-10-11 11:25:44 +0200

    refactor

--- sheets/Author.py
+++
@@ -1,6 +0,0 @@
-class Author(object):
-    def __init__(self, fullname="", country="",institute=""):
-        self.fullname=fullname
-        self.country=country
-        self.institute=institute
-    
--- sheets/Note.py
+++
@@ -1,6 +0,0 @@
-class Note(object):
-    def __init__(self, code=0, submitter=None):
-        self.code=code
-        self.submitter=submitter
-        
-    
