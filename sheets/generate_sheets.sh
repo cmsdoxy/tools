22f56a61e20501df9220b80777756edbb6ddc387
cmsdoxy <cmsdoxy@cern.ch>
2013-10-14 10:59:07 +0200

    Update generate_sheets.sh

--- sheets/generate_sheets.sh
+++ sheets/generate_sheets.sh
@@ -2,10 +2,14 @@
 
 python scripts/prerequisites.py
 
+echo "Generating Sheet1"
 python scripts/sheet1Main.py
 
+echo "Generating Sheet2"
 python scripts/sheet2Main.py
 
+echo "Generating Sheet4"
 python scripts/sheet3Main.py
 
+echo "Generating Sheet4"
 python scripts/sheet4Main.py
