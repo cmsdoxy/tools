d2ef8247e2c6e35e4917450df0cae8c8833de3b9
Mehmet <mehmet3@gmail.com>
2013-10-17 14:28:49 +0200

    Rename

--- refman/scripts/bash_script.sh
+++ refman/scripts/bash_script.sh
@@ -1,13 +1,13 @@
 #!/bin/tcsh
 set PATH = "/afs/cern.ch/work/a/aaltunda/public/www/doc/html/"
 
-time python MPG/run.py $PATH index.html
+time python MainPageGenerator/run.py $PATH index.html
 echo
-time python Class\ and\ Namespace\ Splitter/run.py $PATH annotated.html annotatedList_
+time python ClassNamespaceSplitter/run.py $PATH annotated.html annotatedList_
 echo
-time python Config\ Files/run.py $PATH configfiles.html
+time python ConfigFiles/run.py $PATH configfiles.html
 echo
-time python Class\ and\ Namespace\ Splitter/run.py $PATH namespaces.html namespacesList_ 
+time python ClassNamespaceSplitter/run.py $PATH namespaces.html namespacesList_ 
 echo
-time python Package\ Splitter/run.py $PATH pages.html packageDocumentation_
+time python PackageSplitter/run.py $PATH pages.html packageDocumentation_
 echo
